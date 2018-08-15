97c3c755be74b39bbe9c46756d9629f80c436c58
Hevay <89835215@163.com>
2018-08-15 15:35:33 +0800

    master分支先一步推送到远程仓库

--- Test1.java
+++ Test1.java
@@ -2,5 +2,6 @@ class Test1{
 	public static void main(String[] args){
 		String s = "currency";//,解决冲突,改成通用 "currency"
 		System.out.println("增加代码后再次提交到github远程仓库");
+		System.out.println("master分支增加一行代码");
 	}
 }
