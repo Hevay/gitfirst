a3f998687fbb2ba52b732754d008786565abadac
Hevay <89835215@163.com>
2018-08-15 14:44:11 +0800

    修改代码后，再次提交到github远程仓库

--- Test1.java
+++ Test1.java
@@ -1,5 +1,6 @@
 class Test1{
 	public static void main(String[] args){
-		String s = "currency" //,解决冲突,改成通用 "currency"
+		String s = "currency"; //,解决冲突,改成通用 "currency"
+		println("增加代码后再次提交到github远程仓库");
 	}
 }
